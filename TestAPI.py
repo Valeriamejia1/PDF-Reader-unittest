3c56426f797a698a72834b8a3f0f023e617c4a5f
Valeriamejia1 <128627556+Valeriamejia1@users.noreply.github.com>
2023-06-30 10:04:20 -0600

    Add files via upload

---
+++ TestAPI.py
@@ -0,0 +1,21 @@
+import unittest
+import pandas as pd
+
+class TestExcel(unittest.TestCase):
+
+    def testCase1(self):
+
+        # Lee el archivo de Excel y selecciona el sheet "OutputData"
+        data_frame = pd.read_excel("OutputAPI.xlsx", sheet_name="OutputData")
+
+        # Obtiene la columna deseada
+        columna = data_frame["PAYCODE"]
+
+        # Verifica los valores esperados
+        valores_esperados = ["LUNCH", "LCUP"]
+
+        for valor in columna:
+            self.assertNotIn(valor, valores_esperados, f"El valor '{valor}' está presente en la columna PAYCODE")
+
+if __name__ == '__main__':
+    unittest.main()
