0bb88e6e9311a57b02bf9324133c0561222d80f1
Valeria Mejia <Valeria.Mejia@ayahealthcare.com>
2023-07-24 16:23:38 -0600

    TC 13

--- TestCasesDefault/TestCasesDefault.py
+++ TestCasesDefault/TestCasesDefault.py
@@ -320,7 +320,7 @@ class ExcelTestCase(unittest.TestCase):
     def test_Default_9(self): 
         # Files to validate
         # Desc: Date without + or -
-        file_paths = ['TestCasesDefault\\Aya-WE 8.28.22.xlsx', 'TestCasesDefault\\Aya-WE 8.28.22 Minutes.xlsx']
+        file_paths = ['TestCasesDEFAULT/Aya-WE 8.28.22.xlsx', 'TestCasesDEFAULT/Aya-WE 8.28.22 Minutes.xlsx']
         sheet_name = 'Sheet1'
 
         # Regular expression to check for valid date format 'mm/dd/yyyy' and to detect '+' or '-' symbols
@@ -409,7 +409,7 @@ class ExcelTestCase(unittest.TestCase):
     def test_default_11(self): 
         # Files to validate
         #Desc: Date with Dash needs to be display
-        file_paths = ['TestCasesDefault\\Scripps Approved Kronos we 6.25.22 minutes.xlsx', 'TestCasesDefault\\Scripps Approved Kronos we 6.25.22.xlsx']
+        file_paths = ['TestCasesDEFAULT/Scripps Approved Kronos we 6.25.22 minutes.xlsx', 'TestCasesDEFAULT/Scripps Approved Kronos we 6.25.22.xlsx']
         sheet_name = 'Sheet1'
 
         # Regular expression to check for valid date format 'mm/dd/yyyy' and to detect '+' or '-' symbols
@@ -497,6 +497,55 @@ class ExcelTestCase(unittest.TestCase):
             if correct_files:
                 print(f"TEST 12 DEFAULT CORRECT: Column Date format is correct in both files Time Detail_100822-102122")
 
+    def test_DEFAULT_13(self):
+        # Add the 'self' parameter
+        # List of file paths and sheet names to validate
+        file_data = [
+            {'file_path': 'TestCasesDefault/6-11.xlsx', 'sheet_name': 'Sheet1'},
+            {'file_path': 'TestCasesDefault/6-11 Minutes.xlsx', 'sheet_name': 'Sheet1'}
+        ]
+
+        # List to store all error messages
+        all_error_messages = []
+        correct_files = []
+
+        for data in file_data:
+            file_path = data['file_path']
+            sheet_name = data['sheet_name']
+
+            # Read the Excel file and the specified sheet
+            try:
+                df = pd.read_excel(file_path, sheet_name=sheet_name)
+
+                # Check for missing or invalid dates
+                error_messages = []  # Collect error messages for this file
+                for index, value in df['DATE'].items():
+                    if pd.isnull(value):  # Check for empty cells
+                        error_messages.append(f"TEST 13 DEFAULT INCORRECT: Empty cell found in row {index + 2}, column DATE. File: {file_path}")
+                    else:
+                        date_str = str(value)
+                        # Use regular expression to check for valid date format 'mm/dd/yyyy' or 'dd/mm/yyyy'
+                        if not re.match(r'^(\d{1,2}/\d{1,2}/\d{4})|(\d{1,2}-\d{1,2}-\d{4})$', date_str):
+                            error_messages.append(f"TEST 13 DEFAULT INCORRECT: Invalid date format found in row {index + 2}, column DATE: '{date_str}'. File: {file_path}")
+
+                if error_messages:  # If there are errors for this file, add them to the all_error_messages list
+                    all_error_messages.extend(error_messages)
+                else:
+                    correct_files.append(file_path)
+            except FileNotFoundError as fnf:
+                self.fail(f"Error: File '{file_path}' not found.")
+            except Exception as e:
+                self.fail(f"Unexpected error while processing the file '{file_path}': {str(e)}")
+
+        # Raise a single failure with all the collected error messages
+        if all_error_messages:
+            error_message = "\n".join(all_error_messages)
+            self.fail(error_message)
+        else:
+            if correct_files:
+                correct_file_message = "\n".join(correct_files)
+                print(f"TEST 13 DEFAULT CORRECT: Column Date format is correct in both files 6-11")
+
     def test_DEFAULT_14(self):
         # File path and name of the Excel file
         excel_file = 'TestCasesDEFAULT/6-11.xlsx'
@@ -553,9 +602,9 @@ class ExcelTestCase(unittest.TestCase):
         print(".TEST 12 DEFAULT CORRECT: The nurse registered in the file three shifts.")
 
     def test_DEFAULT_16(self):
-        #Files: TestCasesDEFAULT\6-11 Minutes.xlsx , TestCasesDEFAULT\6-11.xlsx
+        #Files: TestCasesDEFAULT/6-11 Minutes.xlsx , TestCasesDEFAULT/6-11.xlsx
         # Descriotion: Check if the nurse has her last comment that is located in the next page
-        file_paths = ['TestCasesDEFAULT\\6-11.xlsx', 'TestCasesDEFAULT\\6-11 Minutes.xlsx']
+        file_paths = ['TestCasesDEFAULT/6-11.xlsx', 'TestCasesDEFAULT/6-11 Minutes.xlsx']
 
         # Name to search and data to verify in the 'Comments' column
         name_to_find = 'Casey, Quentasha'
--- TestCasesDefault/vale.py
+++ TestCasesDefault/vale.py
@@ -3,12 +3,12 @@ import pandas as pd
 import re
 
 class TestGLCode(unittest.TestCase):
-    def test_DEFAULT_12(self):
+    def test_DEFAULT_13(self):
         # Add the 'self' parameter
         # List of file paths and sheet names to validate
         file_data = [
-            {'file_path': 'TestCasesDefault/Time Detail_100822-102122 minutes.xlsx', 'sheet_name': 'Sheet1'},
-            {'file_path': 'TestCasesDefault/Time Detail_100822-102122.xlsx', 'sheet_name': 'Sheet1'}
+            {'file_path': 'TestCasesDefault/6-11.xlsx', 'sheet_name': 'Sheet1'},
+            {'file_path': 'TestCasesDefault/6-11 Minutes.xlsx', 'sheet_name': 'Sheet1'}
         ]
 
         # List to store all error messages
@@ -27,12 +27,12 @@ class TestGLCode(unittest.TestCase):
                 error_messages = []  # Collect error messages for this file
                 for index, value in df['DATE'].items():
                     if pd.isnull(value):  # Check for empty cells
-                        error_messages.append(f"TEST 12 DEFAULT INCORRECT: Empty cell found in row {index + 2}, column DATE. File: {file_path}")
+                        error_messages.append(f"TEST 13 DEFAULT INCORRECT: Empty cell found in row {index + 2}, column DATE. File: {file_path}")
                     else:
                         date_str = str(value)
                         # Use regular expression to check for valid date format 'mm/dd/yyyy' or 'dd/mm/yyyy'
                         if not re.match(r'^(\d{1,2}/\d{1,2}/\d{4})|(\d{1,2}-\d{1,2}-\d{4})$', date_str):
-                            error_messages.append(f"TEST 12 DEFAULT INCORRECT: Invalid date format found in row {index + 2}, column DATE: '{date_str}'. File: {file_path}")
+                            error_messages.append(f"TEST 13 DEFAULT INCORRECT: Invalid date format found in row {index + 2}, column DATE: '{date_str}'. File: {file_path}")
 
                 if error_messages:  # If there are errors for this file, add them to the all_error_messages list
                     all_error_messages.extend(error_messages)
@@ -50,7 +50,7 @@ class TestGLCode(unittest.TestCase):
         else:
             if correct_files:
                 correct_file_message = "\n".join(correct_files)
-                print(f"TEST 12 DEFAULT CORRECT: Column Date format is correct in both files")
+                print(f"TEST 13 DEFAULT CORRECT: Column Date format is correct in both files 6-11")
 
 if __name__ == "__main__":
     unittest.main()
