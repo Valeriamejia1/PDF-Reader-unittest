ae5067da76bd12cf2153ff56142e07f806be3630
Valeria Mejia <Valeria.Mejia@ayahealthcare.com>
2023-07-25 16:08:12 -0600

    test

--- Funtions/helloWorld.py
+++ Funtions/helloWorld.py
@@ -1,5 +1,12 @@
 def imprimir_hello_world():
-    print("Hello, World!")
+    print("Hello, World!\n")
+
+
+def imprimir_test():
+    print("test\n")
 
 # Llamamos a la función para imprimir el mensaje
 imprimir_hello_world()
+imprimir_test()
+
+
