d2631ab16e8cd77fadede7c56af4010be32c53ac
Valeria Mejia <Valeria.Mejia@ayahealthcare.com>
2023-07-27 15:11:46 -0600

    t

--- Funtions/helloWorld.py
+++ Funtions/helloWorld.py
@@ -4,30 +4,24 @@ def imprimir_hello_world():
 def imprimir_test():
     print("test\n")
 
-def suma_de_dos_numeros():
+def suma_de_dos_Numeros(Numero1, Numero2):
     try:
         # Pedir al usuario que ingrese los dos números
         Numero1 = float(input("Ingresa el primer número: "))
         Numero2 = float(input("Ingresa el segundo número: "))
 
         # Sumar los dos números ingresados
-        suma = numero1 + numero2
+        suma = Numero1 + Numero2
 
         # Mostrar el resultado
-        print(f"La suma de {numero1} y {numero2} es: {suma}")
+        print(f"La suma de {Numero1} y {Numero2} es: {suma}")
 
     except ValueError:
         print("Error: Asegúrate de ingresar números válidos.")
-
-# Llamar a la función para ejecutarla
-
-
-
-
 
 # Llamamos a la función para imprimir el mensaje
 if __name__ == "__main__":
     imprimir_hello_world()
     imprimir_test()
-    suma_de_dos_numeros()
+    suma_de_dos_Numeros()
 
