00edc36fb30f5f431d816bafea9618e3138d3328
Valeria Mejia <Valeria.Mejia@ayahealthcare.com>
2023-08-09 10:44:02 -0600

    commin

--- TestCasesUKGCommon/TestCasesUKGCommon.py
+++ TestCasesUKGCommon/TestCasesUKGCommon.py
@@ -6,7 +6,7 @@ class ExcelTest(unittest.TestCase):
     def test_UKGC_1(self):
 
         # Loads the Excel file into a DataFram
-        df = pd.read_excel('TestCasesUKGCommon/UKG Common Empty.xlsx', header=None)
+        df = pd.read_excel('OUTPUT UKGCommon/UKG Common Empty.xlsx', header=None)
 
         # Gets the number of rows with data beyond the headers
         num_data_rows = len(df) - 1 
@@ -22,7 +22,7 @@ class ExcelTest(unittest.TestCase):
         # Description: Check last shift is present
         # File: Martin ppe
         # Upload Excel file
-        excel_file = 'TestCasesUKGCommon/Martin ppe 4.22.23.xlsx'
+        excel_file = 'OUTPUT UKGCommon\Martin ppe 4.22.23.xlsx'
         df = pd.read_excel(excel_file, sheet_name='Sheet1')
 
         # Specify the search criteria
@@ -100,19 +100,19 @@ class ExcelTest(unittest.TestCase):
     #Files: Martin ppe 4.22.23, martin b, time martin a ORIG
 
     def test_UKGC_3_1(self):
-        self.compare_excel_files("TestCasesUKGCommon/Martin ppe 4.22.23 ORIG.xlsx", "TestCasesUKGCommon/Martin ppe 4.22.23.xlsx")
+        self.compare_excel_files("TestCasesUKGCommon/Martin ppe 4.22.23 ORIG.xlsx", "OUTPUT UKGCommon\Martin ppe 4.22.23.xlsx")
         print("TEST 3.1 UKGCommon CORRECT: Martin ppe 4.22.23.xlsx data match the original version")
 
     def test_UKGC_3_2(self):
-        self.compare_excel_files("TestCasesUKGCommon/martin b ORIG.xlsx", "TestCasesUKGCommon/martin b.xlsx")
+        self.compare_excel_files("TestCasesUKGCommon/martin b ORIG.xlsx", "OUTPUT UKGCommon\martin b.xlsx")
         print("TEST 3.2 UKGCommon CORRECT: martin b ORIG.xlsx data match the original version")
 
     def test_UKGC_3_3(self):
-        self.compare_excel_files("TestCasesUKGCommon/martin time a ORIG.xlsx", "TestCasesUKGCommon/martin time a.xlsx")
+        self.compare_excel_files("TestCasesUKGCommon/martin time a ORIG.xlsx", "OUTPUT UKGCommon\martin time a.xlsx")
         print("TEST 3.3 UKGCommon CORRECT: martin time a ORIG.xlsx data match the original version")
 
     def test_UKGC_4(self):
-        excel_file = "TestCasesUKGCommon/Martin ppe 4.22.23.xlsx"
+        excel_file = "OUTPUT UKGCommon\Martin ppe 4.22.23.xlsx"
         sheet_name = "Sheet1"
         data = pd.read_excel(excel_file, sheet_name=sheet_name)
         name_column = "NAME"
@@ -138,7 +138,7 @@ class ExcelTest(unittest.TestCase):
         print("TEST 4 UKGCommon CORRECT: For AUGUSTE, LOURDJINA the comments match those in the PDF")
 
     def test_UKGC_5(self):
-        excel_file = "TestCasesUKGCommon/Martin ppe 4.22.23.xlsx"
+        excel_file = "OUTPUT UKGCommon\Martin ppe 4.22.23.xlsx"
         sheet_name = "Sheet1"
         data = pd.read_excel(excel_file, sheet_name=sheet_name)
         name_column = "NAME"
